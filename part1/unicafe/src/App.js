2d07c61f73facb46e2e332335c47c21092ab0b6b
Nahin27 <nahin271199@gmail.com>
2022-10-25 01:29:25 +0600

    Finished 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,6 +4,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  let total = good + bad + neutral
+  let average = ((good * 1) + (bad*-1)) / total
+  let positive = good / total * 100
   return (
     <div>
       <h1>give feedback</h1>
@@ -17,6 +20,9 @@ const App = () => {
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {total}</p>
+        <p>average {average}</p>
+        <p>positive {positive} %</p>
       </div>
     </div>
   )
